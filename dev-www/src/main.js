37cc57a1b13f0b3b962cc3492cb0ddbfb57df568
Cookie923 <g-dragon923@163.com>
2018-11-17 01:16:52 +0800

    hot layout

--- dev-www/src/main.js
+++ dev-www/src/main.js
@@ -4,11 +4,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import './assets/styles/reset.css'
 import './assets/styles/border.css' // 一像素边框解决方案
+import './assets/styles/iconfont.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(ElementUI)
 
 /* eslint-disable no-new */
 new Vue({
--- dev-www/src/pages/hot/Hot.vue
+++ dev-www/src/pages/hot/Hot.vue
@@ -1,6 +1,22 @@
 <template>
   <div>
-    <header>Lighting</header>
+    <header class="title">
+      <span class="iconfont">&#xe63c;</span>
+        Lighting
+    </header>
+    <el-carousel trigger="click" height="4rem">
+      <el-carousel-item v-for="item in 4" :key="item">
+        <div class="news-title">
+          <h3>影讯标题{{ item }}</h3>
+          <span>小标题电影资讯内容概述</span>
+        </div>
+      </el-carousel-item>
+    </el-carousel>
+    <div class="title title-text">院线热映</div>
+    <div class="theatre">
+      要写一个组件
+    </div>
+    <div class="title title-text comment">热门影评</div>
   </div>
 </template>
 
@@ -9,10 +25,37 @@
 </script>
 
 <style lang="stylus" scoped>
-  header
+  .title
     height: .74rem
+    padding: 0 .1rem
     background: #f8f8f8
     text-align: center
     font-size: .4rem
+    color: #2D445C
     line-height: .74rem
+    .iconfont
+      float: left
+      font-size: .4rem
+      color: #2D445C
+  .el-carousel__item h3 
+    color: #475669;
+    font-size: .3rem;
+    opacity: 0.75;
+    line-height: .7rem;
+    margin: 0;
+  .news-title
+    margin-top: 2.5rem
+    margin-left: .2rem
+  .el-carousel__item:nth-child(2n) 
+     background-color: #99a9bf;
+  
+  .el-carousel__item:nth-child(2n+1) 
+     background-color: #d3dce6;
+  .title-text
+    font-size: .35rem
+    font-weight: bold
+  .comment
+    text-align: left
+  .theatre
+    height: 3.5rem
 </style>
